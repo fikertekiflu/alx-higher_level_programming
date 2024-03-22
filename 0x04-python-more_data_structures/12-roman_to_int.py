04dc58dbafece3345c45dfd743152899e46f3c03
fikertekiflu <casssiopfiker@gmail.com>
2024-03-22 11:37:53 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,11 +5,9 @@ def roman_to_int(roman_string: str):
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     numbers = [data[x] for x in roman_string] + [0]
     rep = 0
-
     for i in range(len(numbers) - 1):
         if numbers[i] >= numbers[i+1]:
             rep += numbers[i]
         else:
             rep -= numbers[i]
-
     return rep
