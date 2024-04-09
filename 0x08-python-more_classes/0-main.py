8dfd42f06a11e9cf0d8c895ff15365f3e4fe89b9
fikertekiflu <casssiopfiker@gmail.com>
2024-04-09 12:20:39 -0700

    rectangle that defenes the rectangles

---
+++ 0x08-python-more_classes/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
+#!/usr/bin/python3
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
+
