f2a4021a73df01958ce73423ab22ac8909af4695
fikertekiflu <casssiopfiker@gmail.com>
2024-05-22 03:23:02 -0700

    oreder by desC

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,8 +1,2 @@
--- script thta creates a table second tbale
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-
--- Script should create maximum of 4 records
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+-- a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, name FROM second_table ORDER BY score DESC;
