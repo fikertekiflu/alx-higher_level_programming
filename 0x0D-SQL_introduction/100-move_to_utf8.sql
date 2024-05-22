7962dbf1869e201d481273172b3338766da616af
fikertekiflu <casssiopfiker@gmail.com>
2024-05-22 03:58:44 -0700

    commited

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- converts hbtn_0c_o database to utf8
+-- table: first _table to utf8
+--  field :name to utf8
+
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name  VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
