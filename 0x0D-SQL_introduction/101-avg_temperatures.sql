f9f92d1f05d701dd7c79f41ba3d9545a84d597d8
fikertekiflu <casssiopfiker@gmail.com>
2024-05-22 04:58:30 -0700

    commited

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
--- displays average temp by city in descending order
-SELECT city, AVE(value) AS avg_temp
-FROM temperatures
-GROUP BY city 
-ORDER BY avg_temp DESC;
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
